be112e274ce92bf61958367e874cd52b44c07ee0
maniaa1822 <manias1822363@studenti.uniroma1.it>
2024-12-03 16:34:32 +0100

    esa-sub-1

--- data/process-mission1.py
+++ data/process-mission1.py
@@ -82,7 +82,12 @@ def filter_channels(input_file,output_directory):
     # Read the CSV input
     df = pd.read_csv(input_file)
     #list of channels
-    channels = [1,10,11,12,13,14,15,16,17,18]
+    channels = [
+        '1', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '2', '20', '21', '22', 
+        '23', '24', '25', '26', '27', '28', '29', '3', '30', '31', '32', '33', '34', '35', '36', 
+        '37', '38', '39', '4', '40', '41', '42', '43', '44', '45', '46', '47', '48', '49', '5', 
+        '50', '51', '52', '53', '54'
+    ]
     #convert the channels to string
     channels = [f'{ch}' for ch in channels]
     # Get channel columns
@@ -99,5 +104,5 @@ def filter_channels(input_file,output_directory):
     
 filter_channels('3_months.train_with_attack.csv',
 
-                'esa-sub')
+                'esa-sub-1')
 # %%
